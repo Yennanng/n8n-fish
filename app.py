46e3d4fd65605b334df5c67ba9bbaf1f93ba8062
Yennanng <yennan275@gmail.com>
2025-06-08 09:11:34 +0700

    sửa

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ if uploaded_file is not None:
     image.save(buffered, format="PNG")
     img_base64 = base64.b64encode(buffered.getvalue()).decode()
 
-    if st.button("📤 Nhận diện bệnh"):
-        webhook_url = "https://n8n.n2nai.io/webhook/my-app"
+    if st.button("📤 Nhận diện bệnh ở cá nha"):
+        webhook_url = "https://n8n.n2nai.io/webhook/fish-image"
 
         response = requests.post(webhook_url, json={
             "filename": uploaded_file.name,
