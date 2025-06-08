28204d5b4018f369835451def22e61a7adab6b94
Yennanng <yennan275@gmail.com>
2025-06-08 09:57:37 +0700

    sua

--- app.py
+++ app.py
@@ -1,34 +1,57 @@
+# import streamlit as st
+# import requests
+# import base64
+# from PIL import Image
+# from io import BytesIO
+#
+#
+#
+# st.set_page_config(page_title="Upload Fish Image", page_icon="🐟")
+# st.title("🐟 Fish Image Uploader")
+#
+# uploaded_file = st.file_uploader("Upload fish image", type=["jpg", "jpeg", "png"])
+#
+# if uploaded_file is not None:
+#     st.image(uploaded_file, caption="Preview", use_column_width=True)
+#
+#     # Đọc và chuyển ảnh thành base64
+#     image = Image.open(uploaded_file)
+#     buffered = BytesIO()
+#     image.save(buffered, format="PNG")
+#     img_base64 = base64.b64encode(buffered.getvalue()).decode()
+#
+#     if st.button("📤 Gửi ảnh đến n8n"):
+#         webhook_url = "https://n8n.n2nai.io/webhook-test/fish-image"
+#
+#         response = requests.post(webhook_url, json={
+#             "filename": uploaded_file.name,
+#             "image_base64": img_base64
+#         })
+#
+#         if response.status_code == 200:
+#             st.success("✅ Ảnh đã được gửi đến n8n!")
+#         else:
+#             st.error("❌ Gửi thất bại.")
+
 import streamlit as st
 import requests
 import base64
-from PIL import Image
-from io import BytesIO
 
+st.title("Nhận diện bệnh cho cá")
 
-
-st.set_page_config(page_title="Upload Fish Image", page_icon="🐟")
-st.title("🐟 Fish Image Uploader")
-
-uploaded_file = st.file_uploader("Upload fish image", type=["jpg", "jpeg", "png"])
+uploaded_file = st.file_uploader("Tải ảnh cá lên", type=["jpg", "png"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Preview", use_column_width=True)
+    st.image(uploaded_file, caption="Ảnh cá cần nhận diện", use_column_width=True)
 
-    # Đọc và chuyển ảnh thành base64
-    image = Image.open(uploaded_file)
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_base64 = base64.b64encode(buffered.getvalue()).decode()
+    if st.button("Nhận diện"):
+        img_bytes = uploaded_file.read()
+        img_base64 = base64.b64encode(img_bytes).decode()
 
-    if st.button("📤 Gửi ảnh đến n8n"):
-        webhook_url = "https://n8n.n2nai.io/webhook-test/fish-image"
+        payload = {"image_base64": img_base64}
+        response = requests.post("http://localhost:5678/webhook/fish-disease-recognition", json=payload)
 
-        response = requests.post(webhook_url, json={
-            "filename": uploaded_file.name,
-            "image_base64": img_base64
-        })
-
-        if response.status_code == 200:
-            st.success("✅ Ảnh đã được gửi đến n8n!")
+        if response.ok:
+            st.success("✅ Kết quả: " + response.json()["message"])
         else:
-            st.error("❌ Gửi thất bại.")
+            st.error("❌ Lỗi khi gửi ảnh tới AI agent.")
